6e3d4ce2af1810a66de803de9dc6d22ba4af3eb7
Rahoni Couto <djlokijoker@gmail.com>
2020-07-29 08:20:52 -0300

    first commit - imersao Reack

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Menu from './Componentes/Menu';
+import dadosIniciais from './data/dados_iniciais.json'
+import BannerMain from './Componentes/BannerMain';
+import Carousel from './Componentes/Carousel';
+import Footer from './Componentes/Footer';
+
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <Menu />
+        
+        <BannerMain
+          videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+          url={dadosIniciais.categorias[0].videos[0].url}
+          videosDescription={"O que é front-end?"}
+        />
+      </header>
+
+      <main>
+        <Carousel 
+          ignoreFirstVideo
+          category={dadosIniciais.categorias[0]}
+        />
+          
+        <Carousel 
+          ignoreFirstVideo
+          category={dadosIniciais.categorias[1]}
+        />
+
+        <Carousel 
+          ignoreFirstVideo
+          category={dadosIniciais.categorias[2]}
+        />
+      </main>
+
+      <footer>
+        <Footer/>
+      </footer>
+
+    </div>
+  );
+}
+
+export default App;
